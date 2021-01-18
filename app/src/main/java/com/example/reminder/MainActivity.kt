2689d363f8b59b6e5828806fb3f07da914669aaa
Riikka Valkama <riikka.valkama97@gmail.com>
2021-01-18 15:39:09 +0200

    Initial commit

---
+++ app/src/main/java/com/example/reminder/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.reminder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    private
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
